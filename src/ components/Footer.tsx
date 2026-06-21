626db6147a24aac09423e31b6864c18ae3e766a2
Yoan CHALENCON <contact@yoan-chalencon.fr>
2026-06-21 12:03:03 +0200

    📝 Add start date year in footer

--- src/ components/Footer.tsx
+++ src/ components/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="border-dark/30 dark:border-light/30 border-t py-5">
       <p className="text-dark/50 dark:text-light/50 text-center text-sm">
-        &copy; {new Date().getFullYear()} - Tous droits réservés - Conçu et
-        développé par Yoan CHALENCON
+        &copy; 2023 &ndash; {new Date().getFullYear()} &mdash; Tous droits
+        réservés &mdash; Conçu et développé par Yoan CHALENCON
       </p>
     </footer>
   );
