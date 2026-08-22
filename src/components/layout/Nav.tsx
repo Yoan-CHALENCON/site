f17b0d3dd73e3a09d5a1c850628792da75cf8478
Yoan CHALENCON <contact@yoan-chalencon.fr>
2026-08-22 16:49:59 +0200

    ✨ Update Nav to setup current page info

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,12 +1,15 @@
 import NavLink from "./NavLink";
+import { useLocation } from "react-router-dom";
 
 export default function Nav() {
+  const { pathname } = useLocation();
+
   return (
     <nav className="flex gap-12">
-      <NavLink isCurrentPage={true} title="Accueil" />
-      <NavLink to="/projets" title="Projets" />
-      <NavLink to="/parcours" title="Parcours" />
-      <NavLink to="/contact" title="Contact" />
+      <NavLink to="/" title="Accueil" isCurrentPage={pathname === "/"} />
+      <NavLink to="/projets" title="Projets" isCurrentPage={pathname.startsWith("/projets")} />
+      <NavLink to="/parcours" title="Parcours" isCurrentPage={pathname.startsWith("/parcours")} />
+      <NavLink to="/contact" title="Contact" isCurrentPage={pathname.startsWith("/contact")} />
     </nav>
   );
 }
